00af96751e205b23e9dcb0c0e5ffc91db06585dd
wugren <wuguoren@buckyos.com>
2024-04-25 14:48:36 +0800

    Remove unnecessary dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::fmt::{Debug, Display};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -10,7 +9,7 @@ pub enum SerdeResult<T, E> {
 impl<T, E> From<sfo_result::Result<T, E>> for SerdeResult<T, E>
 where
     T: Serialize + for<'a> Deserialize<'a>,
-    E: Serialize + for<'a> Deserialize<'a> + Debug + Display + Sync + Send + 'static + From<String> {
+    E: Serialize + for<'a> Deserialize<'a> {
     fn from(value: sfo_result::Result<T, E>) -> Self {
         match value {
             Ok(t) => SerdeResult::Ok(t),
@@ -21,7 +20,7 @@ where
 
 impl<T, E> Into<sfo_result::Result<T, E>> for SerdeResult<T, E>
 where T: Serialize + for<'a> Deserialize<'a>,
-      E: Serialize + for<'a> Deserialize<'a> + Send + Sync + std::error::Error + 'static {
+      E: Serialize + for<'a> Deserialize<'a> {
     fn into(self) -> sfo_result::Result<T, E> {
         match self {
             SerdeResult::Ok(t) => Ok(t),
