1232adee3b5f567db2c79b06faff06eead5ce8b9
wugren <wuguoren@buckyos.com>
2024-04-27 18:41:24 +0800

    remove osf-result dependency

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,11 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 pub enum SerdeResult<T, E> {
     Ok(T),
-    Err(sfo_result::Error<E>)
+    Err(E)
 }
 
-impl<T, E> From<sfo_result::Result<T, E>> for SerdeResult<T, E>
-where
-    T: Serialize + for<'a> Deserialize<'a>,
-    E: Serialize + for<'a> Deserialize<'a> {
-    fn from(value: sfo_result::Result<T, E>) -> Self {
+impl<T, E> From<std::result::Result<T, E>> for SerdeResult<T, E> {
+    fn from(value: std::result::Result<T, E>) -> Self {
         match value {
             Ok(t) => SerdeResult::Ok(t),
             Err(e) => SerdeResult::Err(e),
@@ -18,10 +15,8 @@ where
     }
 }
 
-impl<T, E> Into<sfo_result::Result<T, E>> for SerdeResult<T, E>
-where T: Serialize + for<'a> Deserialize<'a>,
-      E: Serialize + for<'a> Deserialize<'a> {
-    fn into(self) -> sfo_result::Result<T, E> {
+impl<T, E> Into<std::result::Result<T, E>> for SerdeResult<T, E> {
+    fn into(self) -> std::result::Result<T, E> {
         match self {
             SerdeResult::Ok(t) => Ok(t),
             SerdeResult::Err(e) => Err(e),
